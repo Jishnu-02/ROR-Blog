974827c80d93a14b46dc97c51f85de7ba233297e
Jishnu <jishnusnair500@gmail.com>
2022-05-23 11:42:27 +0530

    CSS updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-
-$(document).on('turbolinks:load', function() {
-    setTimeout(function() {
-      $('.alert').fadeOut();
-    }, 3000);
-  })
